93dad8061b654a438152f22e050aeaff3f81ed50
Andy <anfernandez@fi.uba.ar>
2022-08-21 09:59:56 -0300

    minor update

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from project import create_app
 from decouple import config
 
 PORT = config("PORT", default=8080, cast=int)
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(debug=True, port=PORT)
+    app.run(debug=DEBUG, port=PORT)
